555dbc598ddf5d77b6e74cf723ec8f54a9c91f23
luciusfsilva <lucianoferreiraofficial@gmail.com>
2025-06-13 23:12:33 -0300

    Inclusão do relacionamento com tasks

--- task-manager-api/src/main/java/com/manager/task_manager_api/model/User.java
+++ task-manager-api/src/main/java/com/manager/task_manager_api/model/User.java
@@ -1,9 +1,11 @@
 package com.manager.task_manager_api.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.manager.task_manager_api.enumTask.UserRoleEnum;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,6 +13,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,6 +51,9 @@ public class User {
 
 	@Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", updatable = false)
 	private LocalDateTime createdAt;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Task> tasks;
 
 	void onCreated() {
 		this.createdAt = LocalDateTime.now();
